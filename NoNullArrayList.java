f301da0eac00efaae593d6ea9335b5fc911c77e2
vli21 <vli21@stuy.edu>
2020-12-14 01:45:51 -0500

    tested all methods and worked

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,5 +6,22 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(int start) {
     super(start);
   }
-
+  public T set(int i, T value) {
+    if (value == null) {
+      throw new IllegalArgumentException("arraylist cannot accept null values");
+    }
+    return super.set(i,value);
+  }
+  public boolean add(T value) {
+    if (value == null) {
+      throw new IllegalArgumentException("arraylist cannot accept null values");
+    }
+    return super.add(value);
+  }
+  public void add(int index, T value) {
+    if (value == null) {
+      throw new IllegalArgumentException("Cannot add null to the ArrayList");
+    }
+    super.add(index, value);
+  }
 }
