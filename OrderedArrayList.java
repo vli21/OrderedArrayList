5fbaa1f5ea04b89f0c4574c3d3fe9c1c02b8d013
vli21 <vli21@stuy.edu>
2020-12-14 02:13:35 -0500

    add works

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,5 +7,21 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int starting) {
     super(starting);
   }
-  
+  public boolean add(T value) {
+   if (value == null) {
+     throw new IllegalArgumentException("null values cannot be accepted");
+   }
+   for (int i = 0; i < size(); i++) {
+     if (value.compareTo(super.get(i)) <= 0) {
+       super.add(i, value);
+       return true;
+     }
+   }
+   super.add(value);
+   return true;
+ }
+
+ public void add(int index, T value) {
+   add(value);
+ }
 }
